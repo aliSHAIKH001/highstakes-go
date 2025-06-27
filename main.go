ab1575953d55628a222c01297e2c343626706162
Ali Shaikh <shahinshaikh603@gmail.com>
2025-06-27 21:57:27 +0530

    Feat: Add getBet function to handle user betting input with balance check

--- main.go
+++ main.go
@@ -15,6 +15,35 @@ func getName() string {
 	return name
 }
 
+func getBet(balance uint) uint {
+	var bet uint
+
+	for true {
+		fmt.Printf("Enter your bet, or 0 to quit (balance = $%d) ", balance)
+		fmt.Scan(&bet)
+
+		if bet > balance {
+			fmt.Println("Bet cannot be larger than balance")
+		} else {
+			break
+		}
+	}
+
+	return bet
+}
+
 func main() {
+	balance := uint(200)
 	getName()
+
+	for balance > 0 {
+		bet := getBet(balance)
+		if bet == 0 {
+			break
+		}
+		balance -= bet	
+	}
+
+	fmt.Printf("You left with, $%d.\n", balance)
+	
 }
