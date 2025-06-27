363381f2fe9174df7d14ddf82f111b983a16cad0
Ali Shaikh <shahinshaikh603@gmail.com>
2025-06-27 23:08:49 +0530

    Implemented generateSymbolArray function

--- main.go
+++ main.go
@@ -32,7 +32,35 @@ func getBet(balance uint) uint {
 	return bet
 }
 
+func generateSymbolArray(symbols map[string]uint) []string {
+	symbolArr := []string{}
+	for symbol, count := range symbols {
+		for i := uint(0); i < count; i++ {
+			symbolArr = append(symbolArr, symbol)
+		}
+	}
+	return symbolArr
+}
+
 func main() {
+	symbols := map[string]uint{
+		"A": 4,
+		"B": 7,
+		"C": 12,
+		"D": 20,
+	}
+	// multipliers := map[string]uint{
+	// 	"A": 20,
+	// 	"B": 10,
+	// 	"C": 5,
+	// 	"D": 2, 
+	// }
+
+	symbolArr := generateSymbolArray(symbols)
+	fmt.Println(symbolArr)
+
+
+
 	balance := uint(200)
 	getName()
 
